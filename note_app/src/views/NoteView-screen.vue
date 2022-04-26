d880eabb12865b50e15f4a71e1846dfa4b4af82a
gorskik <56255569+gorskik@users.noreply.github.com>
2022-04-26 21:58:36 +0200

    note page shows text content  and title

--- note_app/src/views/NoteView-screen.vue
+++ note_app/src/views/NoteView-screen.vue
@@ -1,19 +1,44 @@
 
 <script>
-export default {
-    data() {
-        return {
-            id: this.$route.params.id
-        }
-    },
-    
-    methods : {
-     
-    }
+import { doc, getDoc } from "firebase/firestore";
+import { db, auth } from "../main.js";
 
-}
+export default {
+  data() {
+    return {
+      note: null,
+      id: this.$route.params.id,
+      auth: auth,
+      uid: auth.currentUser.uid,
+      title: null,
+      content: null,
+    };
+  },
+  async mounted() {
+    const docRef = doc(db, "users", `${this.uid}`, "notes", `${this.id}`);
+    const docSnap = await getDoc(docRef);
+
+    // this.note =  docSnap;
+
+    if (docSnap.exists()) {
+      console.log("Document data:", docSnap.data());
+        this.title = docSnap.data().Title
+        this.content = docSnap.data().Content
+
+    } else {
+      // doc.data() will be undefined in this case
+      console.log("No such document!");
+    }
+  },
+
+  methods: {},
+};
 </script>
  
 <template>
-  <h1>Note id: {{id}}:</h1>
+  <div class="container">
+    <h1>Note id: {{ id }}:</h1>
+    <h1>Note title: {{ title}}</h1>
+    <h2>Content: {{content}}</h2>
+  </div>
 </template>
