3de2a91adc47ae7479adb79ee6945f1bc1e21b1e
dborowy <dominikborowy@gmail.com>
2022-04-27 13:11:58 +0200

    Fixed PWA

--- note_app/vue.config.js
+++ note_app/vue.config.js
@@ -14,7 +14,26 @@ module.exports = defineConfig({
       msTileImage: 'img/icons/note-144x144.png'
     },
     manifestOptions: {
-      start_url: '/'
+      id: '/',
+      icons: [{
+        src: "img/icons/note-16x16.png",
+          sizes: "16x16",
+          type: "image/png"
+        }, {
+          src: "img/icons/note-32x32.png",
+          sizes: "32x32",
+          type: "image/png"
+        }, 
+        {
+          src: "img/icons/note-144x144.png",
+          sizes: "144x144",
+          type: "image/png"
+        },
+        {
+          src: "img/icons/note-152x152.png",
+          sizes: "152x152",
+          type: "image/png"
+        }],
     }
   }
 })
