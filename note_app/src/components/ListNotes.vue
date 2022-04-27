6ed9d44d1d386f99778e5d1a4a128a37160d61ce
dborowy <56255618+dborowy@users.noreply.github.com>
2022-04-27 13:51:24 +0200

    Update ListNotes.vue

--- note_app/src/components/ListNotes.vue
+++ note_app/src/components/ListNotes.vue
@@ -1,108 +1,83 @@
-<script>
-import { collection, getDocs } from "firebase/firestore"; 
-import {db} from "../main.js";
-import {getAuth} from "firebase/auth";
-import { getStorage, ref, listAll, getDownloadURL } from "firebase/storage";
+import { createRouter, createWebHistory } from "vue-router";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+// import bootstrap from "bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-export default {
-    data() {
-        return {
-            text_notes: [],
-            photo_notes: [],
-            recording_notes: []
-        }
-    },
-    async mounted() {
-        const auth = getAuth();
-
-        const querySnapshot = await getDocs(collection(db, `users/${auth.currentUser.uid}/notes`));
-
-        querySnapshot.forEach((doc) => {
-            console.log(`${doc.id} => ${doc.data()}`);
-            this.text_notes.push(doc)
-        });
-        const storage = getStorage();
-        const photoListRef = ref(storage, `users/${auth.currentUser.uid}/photos`);
-        const photoStorageSnapshot = await listAll(photoListRef);
-        photoStorageSnapshot.items.forEach((itemRef) => {
-            getDownloadURL(ref(storage, itemRef._location.path_)).then((file_url) => {
-                console.log(`item: ${itemRef.name}, url: ${file_url}`);
-                const file_object = {
-                    title: itemRef.name,
-                    url: file_url
-                };
-                this.photo_notes.push(file_object);
-            })
-
-        })
-        const recordingListRef = ref(storage, `users/${auth.currentUser.uid}/recordings`);
-        const recordingStorageSnapshot = await listAll(recordingListRef);
-        recordingStorageSnapshot.items.forEach((itemRef) => {
-            getDownloadURL(ref(storage, itemRef._location.path_)).then((file_url) => {
-                console.log(`item: ${itemRef.name}, url: ${file_url}`);
-                const file_object = {
-                    title: itemRef.name,
-                    url: file_url
-                };
-                this.recording_notes.push(file_object);
-            })
-
-        })
-
-
-    },
-    methods: {
-        first20: function (string) {
-            let split_string = string.split(' ');
-            let result = split_string.slice(0, 20).join(' ')
-            if (split_string.length > 20) {
-                result = result + ' ...'
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: "/", component: () => import("../views/SignIn-screen.vue") },
+        { path: "/register", component: () => import("../views/Register-screen.vue") },
+        { path: "/sign-in", component: () => import("../views/SignIn-screen.vue") },
+        // { path: "/home", component: () => import("../views/home-screen.vue") },
+        { path: "/add-note", component: () => import("../views/NewNote-screen.vue") },
+        { path: "/add-text-note", component: () => import("../views/NewTextNote-screen.vue") },
+        
+        {
+            path: "/home", component: () => import("../views/home-screen.vue"), meta: {
+                requiresAuth: true,
             }
-            return result
         },
+       
+        {
+            path: "/create-photo", component: () => import("../views/CreatePhotoNote-screen.vue"), meta: {
+                requiresAuth: true,
+            }
+        },
+        {
+            path: "/create-text", component: () => import("../views/NewTextNote-screen.vue"), meta: {
+                requiresAuth: true,
+            }
+        },
+        {
+            path: "/create-voice", component: () => import("../views/CreateVoiceNote-screen.vue"), meta: {
+                requiresAuth: true,
+            }
+        },
+        {
+            path: "/notes/:id", name:'note', component: () => import("../views/NoteView-screen.vue"), meta: {
+                requiresAuth: true,
+            }
+        },
+        {
+            path: "/notes/:id", name:'photo-note', component: () => import("../views/PhotoNoteView-screen.vue"), meta: {
+                requiresAuth: true,
+            }
+        },
+        {
+            path: "/notes/rec",  component: () => import("../views/RecordingView-screen.vue"), meta: {
+                requiresAuth: true,
+            }
+        },
+       
+    ],
+});
 
-    }
-
+const getCurrentUser = () => {
+    return new Promise((resolve, reject) => {
+        const removeListener = onAuthStateChanged(
+            getAuth(),
+            (user) => {
+                removeListener();
+                resolve(user);
+            },
+            reject
+        )
+    })
 }
-</script>
 
-<template>
-    <div class="mx-auto" >
-        <div class="container-flex" v-if="text_notes">
-            <TransitionGroup name="list">
-            <div class="card mb-1" v-for="note in text_notes" :key="note.data().Title">
-                <router-link :to="{name:'note', params:{ id: note.id}}">
-                    <div class="card-body" style="text-align: justify;text-justify: inter-word;">
-                        <h5 class="card-title">{{ note.data().Title }}</h5>
-                        <p style="color:grey;" class="card-text">{{ first20(note.data().Content)}}</p>
-                    </div>
-                </router-link>
-            </div>
-            </TransitionGroup>
-        </div>
-        <div class="container-flex" v-if="photo_notes">
-            <TransitionGroup name="list">
-            <div class="card mb-1" v-for="photo_note in photo_notes" :key="photo_note.title">
-                <router-link :to="{name:'note', params:{ id: photo_note.title, url: photo_note.url}}">
-                <div class="card-body" style="text-align: justify;text-justify: inter-word;">
-                    <h5 class="card-title">{{ photo_note.title }}</h5>
-                    <img style="max-width:100px" class="img-thumbnail" v-bind:src="photo_note.url">
-                </div>
-                </router-link>
-            </div>
-            </TransitionGroup>
+router.beforeEach(async (to, from, next) => {
+    if (to.matched.some((record) => record.meta.requiresAuth)) {
+        if (await getCurrentUser()) {
+            next()
+        } else {
+            next("/");
+            alert("no access");
+        }
 
-        </div>
-        <div class="container-flex" v-if="recording_notes">
-            <TransitionGroup name="list">
-            <div class="card mb-1" v-for="recording_note in recording_notes" :key="recording_note.title">
-                <div class="card-body" style="text-align: justify;text-justify: inter-word;">
-                    <h5 class="card-title">{{ recording_note.title }}</h5>
-                    <i class="bi bi-soundwave" style="font-size: 2em"></i>
-                </div>
-            </div>
-            </TransitionGroup>
-        </div>
-    </div>
-</template>
+    } else {
+        next();
+    }
+});
+export default router;
